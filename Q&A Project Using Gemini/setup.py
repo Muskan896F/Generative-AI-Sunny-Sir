4cd4dd403666ee63d1ea8f66fa1d0b2bde6fa7c7
Muskan Ara <muskan17ara@gmail.com>
2025-02-07 20:00:22 +0530

    all files

---
+++ Q&A Project Using Gemini/setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'QApplication',
+    version= '0.0.1',
+    author= 'Muskan Ara',
+    author_email= 'muskan17ara@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
