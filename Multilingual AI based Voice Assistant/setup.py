17200073c3a3960104e0dc6ad2788b9f9040bd11
Muskan Ara <muskan17ara@gmail.com>
2025-02-12 20:35:07 +0530

    all files

---
+++ Multilingual AI based Voice Assistant/setup.py
@@ -0,0 +1,8 @@
+from setuptools import find_packages, setup
+
+setup(name="genai AI Project",
+      version="0.0.1",
+      author="Muskan Ara",
+      author_email="muskan17ara@gmail.com",
+      packages=find_packages()
+      )
